fa7d1d27fae87be78d8e8b05cb9eac0ec60dacb9
yashwanthk87 <karmanghatyashwanth@gmail.com>
2026-07-31 15:51:55 +0530

    Uploading copy of LRU cache

---
+++ 2024016529.py
@@ -0,0 +1,28 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict1 = {}
+
+    def get(self, key: int) -> int:
+        if key in self.dict1:
+            val = self.dict1[key]
+            del self.dict1[key]
+            self.dict1[key] = val
+            return self.dict1[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.dict1)<self.capacity:
+            self.dict1[key] = value
+        else:
+            del self.dict1[key]
+            self.dict1[key] = value
+
+new = LRUCache(2)
+
+print(new.get(1))
+new.put(1,3)
+print(new.get(1))
+print(new.get())
